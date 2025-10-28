b676c1e8a1e976de359e175a6543b8ff875ca16f
Igor Sandro <152331268+theiguim@users.noreply.github.com>
2025-10-28 18:52:53 -0300

    Update Supabase client initialization environment variables

--- src/lib/supabase/server.js
+++ src/lib/supabase/server.js
@@ -12,8 +12,8 @@ export const createServer = (cookieStore) => {
 
  // Cria o cliente Supabase para o servidor
  return createServerClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  process.env.PUBLIC_SUPABASE_URL,
+  process.env.PUBLIC_SUPABASE_ANON_KEY,
   {
    cookies: {
     get(name) {
