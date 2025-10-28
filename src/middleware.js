55dd66990156151a060355790ce0db1678dc5c8f
Igor Sandro <152331268+theiguim@users.noreply.github.com>
2025-10-28 18:53:18 -0300

    Update Supabase environment variable references

--- src/middleware.js
+++ src/middleware.js
@@ -7,8 +7,8 @@ export async function middleware(req) {
 
   // 1. Crie o cliente Supabase para o servidor (Middleware)
   const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    process.env.PUBLIC_SUPABASE_URL,
+    process.env.PUBLIC_SUPABASE_ANON_KEY,
     {
       cookies: {
         get(name) {
